072f555c7e884cb64cbb44fe8726245ca50230e2
DeokHo Jeong <jeongdeokho98@gmail.com>
2023-05-15 11:28:26 +0900

    first commit

---
+++ SwiftUiStudy/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  SwiftUiStudy
+//
+//  Created by Jeong Deokho on 2023/05/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            asdasdasdasd
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUiStudy/SwiftUiStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiStudyApp.swift
+//  SwiftUiStudy
+//
+//  Created by Jeong Deokho on 2023/05/15.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
